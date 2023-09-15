6a4ee94bab1b10372738ed22f13d3a1d47d44e24
Bernard Swanepoel <104361159+XoXoTheFrozenFox@users.noreply.github.com>
2023-09-15 11:27:08 +0200

    i COM MIT LEKKE CODE
    Commited. Please take this as a joke. Im so tired and still need to learn for OS UwU daddy.

--- SuperStore P3/Controllers/OrdersController.cs
+++ SuperStore P3/Controllers/OrdersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Models;
-using EcoPower_Logistics.Repository; // Add the appropriate namespace for your repository
+using EcoPower_Logistics.Repository;
 
 namespace Controllers
 {
@@ -14,22 +14,20 @@ namespace Controllers
     public class OrdersController : Controller
     {
         private readonly IOrdersRepository _ordersRepository;
+        private readonly ICustomersRepository _customersRepository;
 
-        // Constructor to inject the IOrdersRepository
-        public OrdersController(IOrdersRepository ordersRepository)
+        public OrdersController(IOrdersRepository ordersRepository, ICustomersRepository customersRepository)
         {
             _ordersRepository = ordersRepository;
+            _customersRepository = customersRepository;
         }
 
-        // GET: Orders
         public IActionResult Index()
         {
-            // Retrieve a list of orders from the repository and pass it to the view
             var orders = _ordersRepository.GetAll();
             return View(orders);
         }
 
-        // GET: Orders/Details/5
         public IActionResult Details(int? id)
         {
             if (id == null)
@@ -37,7 +35,6 @@ namespace Controllers
                 return NotFound();
             }
 
-            // Retrieve a specific order by ID from the repository
             var order = _ordersRepository.GetById(id.Value);
 
             if (order == null)
@@ -45,34 +42,29 @@ namespace Controllers
                 return NotFound();
             }
 
-            // Display the order details in the view
             return View(order);
         }
 
-        // GET: Orders/Create
         public IActionResult Create()
         {
-            // Display the Create order view
+            ViewBag.CustomerId = new SelectList(_customersRepository.GetAll(), "CustomerId", "CustomerName");
             return View();
         }
 
-        // POST: Orders/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create([Bind("OrderId,OrderDate,CustomerId,DeliveryAddress")] Order order)
         {
             if (ModelState.IsValid)
             {
-                // Create a new order in the repository if the model is valid
                 _ordersRepository.Create(order);
                 return RedirectToAction(nameof(Index));
             }
 
-            // Return the Create view with validation errors if the model is not valid
+            ViewBag.CustomerId = new SelectList(_customersRepository.GetAll(), "CustomerId", "CustomerName", order.CustomerId);
             return View(order);
         }
 
-        // GET: Orders/Edit/5
         public IActionResult Edit(int? id)
         {
             if (id == null)
@@ -80,7 +72,6 @@ namespace Controllers
                 return NotFound();
             }
 
-            // Retrieve a specific order by ID from the repository for editing
             var order = _ordersRepository.GetById(id.Value);
 
             if (order == null)
@@ -88,11 +79,10 @@ namespace Controllers
                 return NotFound();
             }
 
-            // Display the Edit order view
+            ViewBag.CustomerId = new SelectList(_customersRepository.GetAll(), "CustomerId", "CustomerName", order.CustomerId);
             return View(order);
         }
 
-        // POST: Orders/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, [Bind("OrderId,OrderDate,CustomerId,DeliveryAddress")] Order order)
@@ -104,16 +94,14 @@ namespace Controllers
 
             if (ModelState.IsValid)
             {
-                // Update the order in the repository if the model is valid
                 _ordersRepository.Edit(order);
                 return RedirectToAction(nameof(Index));
             }
 
-            // Return the Edit view with validation errors if the model is not valid
+            ViewBag.CustomerId = new SelectList(_customersRepository.GetAll(), "CustomerId", "CustomerName", order.CustomerId);
             return View(order);
         }
 
-        // GET: Orders/Delete/5
         public IActionResult Delete(int? id)
         {
             if (id == null)
@@ -121,7 +109,6 @@ namespace Controllers
                 return NotFound();
             }
 
-            // Retrieve a specific order by ID from the repository for deletion
             var order = _ordersRepository.GetById(id.Value);
 
             if (order == null)
@@ -129,11 +116,9 @@ namespace Controllers
                 return NotFound();
             }
 
-            // Display the Delete order view
             return View(order);
         }
 
-        // POST: Orders/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed(int id)
@@ -143,7 +128,6 @@ namespace Controllers
                 return NotFound();
             }
 
-            // Delete the order from the repository
             _ordersRepository.Delete(id);
 
             return RedirectToAction(nameof(Index));
